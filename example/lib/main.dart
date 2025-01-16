739b7631e004bb1fd4a226103170b8f99692c0d3
BoyPhanna <kimphanna56@gmail.com>
2025-01-16 12:30:37 +0700

    fix select text to delete

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -63,107 +63,87 @@ class _MyHomePageState extends State<MyHomePage> {
                 keyboardType: TextInputType.none,
                 controller: controllerField03,
                 onTap: () {
-                  setState(() {
-                    isKeyboardVisible = true;
-                    controllerKeyboard = controllerField03;
-                    typeLayout = TypeLayout.numeric;
-                  });
-
-                  showDialog(
-                    barrierDismissible: true,
-                    context: context,
-                    barrierColor: Colors.transparent,
-                    builder: (context) {
-                      return Dialog(
-                        alignment: Alignment.bottomCenter,
-                        insetPadding: EdgeInsets.only(right: 8),
-                        backgroundColor: Colors.transparent,
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.pop(context);
-                          },
-                          child: Container(
-                            width: double.infinity,
-                            height: 700,
-                            color: Colors.white.withAlpha(50),
-                            child: Row(children: [
-                              SizedBox(
-                                width: 8,
-                              ),
-                              Expanded(
-                                flex: 5,
-                                child: Container(
-                                  color: Colors.purpleAccent,
-                                  height: double.infinity,
-                                  width:
-                                      MediaQuery.of(context).size.width * .70,
-                                  child: Center(child: Text("Content")),
-                                ),
-                              ),
-                              SizedBox(
-                                width: 8,
-                              ),
-                              Expanded(
-                                flex: 2,
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    Expanded(
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(8),
-                                            color: Colors.white.withAlpha(50),
-                                            border: Border.all(
-                                                color: Colors.pinkAccent)),
-                                        child: Column(
-                                          children: [
-                                            TextField(
-                                              decoration: const InputDecoration(
-                                                contentPadding:
-                                                    EdgeInsets.only(left: 8),
-                                              ),
-                                              controller: controllerKeyboard,
-                                            ),
-                                            VirtualKeyboard(
-                                              height: 650,
-                                              width: double.infinity,
-                                              iconColor: Colors.black,
-                                              textStyle: TextStyle(
-                                                  color: Colors.black,
-                                                  fontSize: 50),
-                                              textController:
-                                                  controllerKeyboard,
-                                              defaultLayouts: const [
-                                                VirtualKeyboardDefaultLayouts
-                                                    .English,
-                                              ],
-                                              alwaysCaps: true,
-                                              borderColor:
-                                                  Colors.pink.withAlpha(100),
-                                              type: VirtualKeyboardType.Custom,
-                                              keys: typeLayout.keyboard,
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ]),
-                          ),
-                        ),
-                      );
-                    },
-                  );
+                  _showKeyboardAndTextField(textController: controllerField03);
                 },
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  _showKeyboardAndTextField({required TextEditingController textController}) {
+    showDialog(
+      barrierDismissible: true,
+      context: context,
+      barrierColor: Colors.transparent,
+      builder: (context) {
+        return Dialog(
+          alignment: Alignment.bottomCenter,
+          insetPadding: EdgeInsets.only(right: 8),
+          backgroundColor: Colors.transparent,
+          child: Container(
+            width: double.infinity,
+            height: 700,
+            color: Colors.white.withAlpha(50),
+            child: Row(children: [
+              SizedBox(
+                width: 8,
+              ),
+              Expanded(
+                flex: 5,
+                child: Container(
+                  color: Colors.purpleAccent,
+                  height: double.infinity,
+                  width: MediaQuery.of(context).size.width * .70,
+                  child: Center(child: Text("Content")),
+                ),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+
+              //Numeric Keyboard
+              Expanded(
+                flex: 2,
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.white.withAlpha(50),
+                      border: Border.all(color: Colors.pinkAccent)),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextField(
+                        decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.only(left: 8),
+                        ),
+                        controller: controllerKeyboard,
+                      ),
+                      VirtualKeyboard(
+                        height: 650,
+                        width: double.infinity,
+                        iconColor: Colors.black,
+                        textStyle: TextStyle(color: Colors.black, fontSize: 50),
+                        textController: controllerKeyboard,
+                        defaultLayouts: const [
+                          VirtualKeyboardDefaultLayouts.English,
+                        ],
+                        alwaysCaps: true,
+                        borderColor: Colors.pink.withAlpha(100),
+                        type: VirtualKeyboardType.Custom,
+                        keys: TypeLayout.numeric.keyboard,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ]),
+          ),
+        );
+      },
     );
   }
 
@@ -184,17 +164,11 @@ enum TypeLayout { numeric }
 
 extension TypeLayoutExtension on TypeLayout {
   List<List> get keyboard {
-    switch (this) {
-      case TypeLayout.numeric:
-        return [
-          ["1", "2", "3"],
-          ["4", "5", "6"],
-          ["7", "8", "9"],
-          ["BACKSPACE", "0", "."],
-        ];
-
-      default:
-        return [];
-    }
+    return [
+      ["1", "2", "3"],
+      ["4", "5", "6"],
+      ["7", "8", "9"],
+      ["BACKSPACE", "0", "."],
+    ];
   }
 }
